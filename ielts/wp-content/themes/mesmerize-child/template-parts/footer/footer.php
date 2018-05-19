96e68509dc55cb3606cd06f138bd1edbaa722cee
jerrybuhisan101 <jerrycbuhisan@gmail.com>
2018-05-19 17:42:04 +0700

    added ielts backup files 5-19-2018

---
+++ ielts/wp-content/themes/mesmerize-child/template-parts/footer/footer.php
@@ -0,0 +1,95 @@
+<div <?php echo mesmerize_footer_container('footer-simple') ?>>
+    <div <?php echo mesmerize_footer_background('footer-content center-xs') ?>>
+        <div class="gridContainer">
+	        <div class="row middle-xs footer-content-row">
+	            <div class="footer-content-col col-xs-12">
+	                  <div class="row">
+                   <div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-12 box-bottom-logo" style="text-align: left;">
+                    <div class="box-img-bottom-logo">
+                        <img src="http://ocean.edu.vn/Content/images/v2/logo-bot.png" alt="Ocean Edu" class="">
+                    </div>
+                    <div class="company-bottom">CÔNG TY CỔ PHẦN GIÁO DỤC ĐẠI DƯƠNG</div>
+                    <div class="brand-bottom">Ocean Edu Vietnam</div>
+                    <address class="add-bottom">
+                        <br>
+                        Tòa nhà Ocean Edu, 204 Nguyễn Lương Bằng, Đống Đa, Hà Nội<br>
+                        Tel: <a href="tel:19006494">1900 6494</a> - <a href="tel:02473000333">02473 000 333</a><br>
+                        Email: info_ielts@oceanedu.vn
+                    </address>
+                </div>
+
+
+                <div class="col-md-8 hidden-xs" style="padding-top: 20px;">
+                  
+                    <div class="row">
+                        <div class="col-md-4" style="text-align: left;">
+                            <b class="font-roboto" style="color: #fff;">Ocean Edu Ielts</b>
+                            <ul class="list-bottom">
+                                    <li><a href="#">Giới thiệu</a></li>
+                                    <li><a href="#">Hệ thống chi nhánh</a></li>
+                                    <li><a href="#">Đội ngũ giáo viên</a></li>
+                                   
+                            </ul>
+                        </div>
+                        <div class="col-md-4" style="text-align: left;">
+                            <b class="font-roboto" style="color: #fff;">Góc học tập</b>
+                            <ul class="list-bottom2">
+                                     <li><a href="http://ielts.ocean.edu.vn/category/phuong-phap-luyen-thi/">Phương pháp luyện thi</a></li>
+                                     <li><a href="http://ielts.ocean.edu.vn/category/tai-lieu-ielts/">Tài liệu Ielts</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-4 " style="text-align: left;">
+						
+                           <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FIELTS-Ocean-Edu-938147863019149%2F&tabs&width=340&height=214&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="214" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                        </div>
+                    </div>
+                </div>
+
+
+		</div> <!-- end of row-->
+
+
+
+	</div>
+                    </div>
+	            </div>
+	        </div>
+	    </div>
+    </div>
+</div>
+<div class="footer-credit" style="
+    text-align: center;
+    color: white;
+    background: #151313;
+">Designed and developed by: Jerry Cris Buhisan</div>
+
+<!-- Scripts Facebook and Google Analytics -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-116373815-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-116373815-1');
+</script>
+
+<!-- end scripts for facebook and googel analytics -->
+
+<!-- Google Code for G&#7917;i bi&#7875;u m&#7851;u Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 812224606;
+var google_conversion_label = "761nCMDe7n8Q3qCmgwM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/812224606/?label=761nCMDe7n8Q3qCmgwM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
